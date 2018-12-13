c5f2d3ee6e84c8597e1721981c76b07d4ed11cd8
Jamie Cansdale <jcansdale@github.com>
2018-12-13 11:03:36 +0000

    Add extension methods for logging timings

--- src/GitHub.Logging/Logging/ILoggerExtensions.cs
+++ src/GitHub.Logging/Logging/ILoggerExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using Serilog;
 
 namespace GitHub.Logging
@@ -24,6 +26,36 @@ namespace GitHub.Logging
                 logger.Warning(messageTemplate, propertyValues);
 #pragma warning restore Serilog004
             }
+        }
+
+        public static void Time(this ILogger logger, string name, Action method)
+        {
+            var startTime = DateTime.Now;
+            method();
+            logger.Information("{Name} took {Seconds:0.00} seconds", name, (DateTime.Now - startTime).TotalSeconds);
+        }
+
+        public static T Time<T>(this ILogger logger, string name, Func<T> method)
+        {
+            var startTime = DateTime.Now;
+            var value = method();
+            logger.Information("{Name} took {Seconds:0.00} seconds", name, (DateTime.Now - startTime).TotalSeconds);
+            return value;
+        }
+
+        public static async Task TimeAsync(this ILogger logger, string name, Func<Task> methodAsync)
+        {
+            var startTime = DateTime.Now;
+            await methodAsync().ConfigureAwait(false);
+            logger.Information("{Name} took {Seconds:0.00} seconds", name, (DateTime.Now - startTime).TotalSeconds);
+        }
+
+        public static async Task<T> TimeAsync<T>(this ILogger logger, string name, Func<Task<T>> methodAsync)
+        {
+            var startTime = DateTime.Now;
+            var value = await methodAsync().ConfigureAwait(false);
+            logger.Information("{Name} took {Seconds:0.00} seconds", name, (DateTime.Now - startTime).TotalSeconds);
+            return value;
         }
     }
 }
